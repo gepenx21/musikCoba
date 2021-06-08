3216c93b50b559764a00c72c7a43a59647db282c
gpx-PC <gepenx21@gmail.com>
2021-06-08 15:29:11 +0700

    delete unused

--- app/src/main/java/com/appodeals/musikcoba/MainActivity.java
+++ app/src/main/java/com/appodeals/musikcoba/MainActivity.java
@@ -1,7 +1,6 @@
 package com.appodeals.musikcoba;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
